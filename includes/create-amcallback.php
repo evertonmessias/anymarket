aa784079ea9a80b9711c63e91af66840ff991d09
evertonmessias <everton.messias@gmail.com>
2022-04-22 17:38:32 -0300

    22-04-2022_17:38:32

--- includes/create-amcallback.php
+++ includes/create-amcallback.php
@@ -1,9 +1,15 @@
 <?php
-if (!empty($_GET)) {
+if (!empty($_POST) || !empty($_GET)) {
     include 'Anymarket.php';
-    echo Anymarket::registerdb(json_encode($_GET));
-}
-if (!empty($_POST)) {
-    include 'Anymarket.php';
-    echo Anymarket::registerdb(json_encode($_POST));
+    header("Access-Control-Allow-Origin: *");
+    header('Cache-Control: no-cache, must-revalidate');
+    header("Content-Type: text/plain; charset=UTF-8");
+    header("HTTP/1.1 200 OK");
+    $dados = file_get_contents("php://input");
+    $saida = array(
+        "POST" => $_POST,
+        "GET" => $_GET,
+        "DADOS" => $dados
+    );
+    echo Anymarket::registerdb(json_encode($saida));
 }
