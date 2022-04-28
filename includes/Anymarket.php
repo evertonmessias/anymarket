79e6504737141a941809abd709f34195790f3c17
evertonmessias <everton.messias@gmail.com>
2022-04-28 18:25:07 -0300

    28-04-2022_18:25:07

--- includes/Anymarket.php
+++ includes/Anymarket.php
@@ -1,7 +1,7 @@
 <?php
 class Anymarket
 {
-    public static function create(array $params)
+    public static function create(array $params) // create woocommeerce in anymarket
     {
         $config_url = get_option('anymarket_input_0') . "/orders";
         $config_token = get_option('anymarket_input_1');
@@ -27,7 +27,7 @@ class Anymarket
         return $response;
     }
 
-    public static function products()
+    public static function products() // list product from anymarket
     {
         $config_url = get_option('anymarket_input_0') . "/products";
         $config_token = get_option('anymarket_input_1');
@@ -51,7 +51,31 @@ class Anymarket
         return $response;
     }
 
-    public static function registerdb($data)
+    public static function orders($id) // view one order in anymarket
+    {
+        $config_url = get_option('anymarket_input_0') . "/orders" . "/" . $id;
+        $config_token = get_option('anymarket_input_1');
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $config_url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'gumgaToken: ' . $config_token . '',
+                'Content-Type: application/json'
+            ),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
+    }
+
+    public static function registerdb($data) // register in db
     {
         global $wpdb;
         $table_name = $wpdb->prefix . 'anymarket';
@@ -63,7 +87,7 @@ class Anymarket
         }
     }
 
-    public static function response()
+    public static function response() // list response
     {
         global $wpdb;
         $table_name = $wpdb->prefix . 'anymarket';
--- includes/create-amcallback.php
+++ includes/create-amcallback.php
@@ -5,11 +5,29 @@ if (!empty($_POST) || !empty($_GET)) {
     header('Cache-Control: no-cache, must-revalidate');
     header("Content-Type: text/plain; charset=UTF-8");
     header("HTTP/1.1 200 OK");
-    $dados = file_get_contents("php://input");
-    $saida = array(
-        "POST" => $_POST,
-        "GET" => $_GET,
-        "DADOS" => $dados
-    );
-    echo Anymarket::registerdb(json_encode($saida));
+    $dados = json_decode(file_get_contents("php://input"));
+
+    if ($dados->type == "ORDER") {
+
+        $order_id = $dados->content->id;
+
+        $saida = json_decode(Anymarket::orders($order_id));
+
+        $id = substr($saida->marketPlaceNumber, 4);
+
+        $status = $saida->status;
+
+        $tab_status = array(
+            'PAID_WAITING_SHIP' => 'pending',
+            'CANCELED' => 'cancelled'
+        );
+
+        $order = wc_get_order($id);
+
+        $result = $order->update_status($tab_status[$status]);
+
+        if ($result == 1) {
+            echo Anymarket::registerdb("Update Anymarket; id: " . $id . " , " . $status);
+        }
+    }
 }
